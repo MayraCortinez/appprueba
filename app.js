42b1da80de31e9bc1a8f2f324869556d337a2303
MayraCortinez <mayrautopia@gmail.com>
2022-02-16 10:32:58 -0300

    comandos agregados

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 let process = require("process");
 
 const process = process[4]
+
+/* /* 
+
+Recuento de comandos
+
+- git --version  = Conocer la version de git
+
+- git init // Inicializar un repositorio de git (una sola vez por proyecto)
+
+- git status // Conocer el estado de git
+- git add . // Agregar archivos al seguimiento de git
+- git commit -m"Mensaje del commit" // Guardar archivos de seguimiento en git
+ */
